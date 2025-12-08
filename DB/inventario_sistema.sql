8f35c01ba647f725d19447ae24c28eb18145646f
LinkTrifuerza <g.orduno23@info.uas.edu.mx>
2025-12-08 11:23:01 -0700

    Agregar carpetas PNG y DB con imágenes y script SQL

---
+++ DB/inventario_sistema.sql
@@ -0,0 +1,370 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-12-2025 a las 18:42:43
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario_sistema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_orden_proveedor`
+--
+
+CREATE TABLE `detalle_orden_proveedor` (
+  `id_detalle` int(11) NOT NULL,
+  `id_orden` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `costo_unitario` decimal(10,2) NOT NULL,
+  `subtotal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta_prueba`
+--
+
+CREATE TABLE `detalle_venta_prueba` (
+  `id_detalle` int(11) NOT NULL,
+  `id_venta` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_unitario` decimal(10,2) NOT NULL,
+  `subtotal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id_empleado` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `rol` enum('ADMINISTRADOR','EMPLEADO') NOT NULL DEFAULT 'EMPLEADO',
+  `correo` varchar(150) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `estatus` enum('ACTIVO','INACTIVO') NOT NULL DEFAULT 'ACTIVO',
+  `puesto` varchar(100) DEFAULT NULL,
+  `horario` varchar(100) DEFAULT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id_empleado`, `nombre`, `apellido`, `usuario`, `password_hash`, `rol`, `correo`, `telefono`, `estatus`, `puesto`, `horario`, `fecha_registro`) VALUES
+(1, 'Gustavo ', 'Orduño Angulo', 'Gusgus', '$2y$10$DJOemxhIgxHib5hqE1MrXuNliCYCNCgRXf3n5pAyQWW418FXzxUxq', 'ADMINISTRADOR', 'gusordu100@gmail.com', '6671995960', 'ACTIVO', 'Administrador', '9:00-18:00', '2025-12-04 16:06:23'),
+(2, 'Susana Valentina Vega Garcia', 'Vega Garcia', 'Susan', '$2y$10$07pJrf.aP9ngkm39SANvlOuRezfTgff7r2vdrYuDbBEaW2sVfQm3q', 'ADMINISTRADOR', 'sv.vega23@info.uas.edu.mx', '6671108991', 'ACTIVO', 'Administrador', '9:00-18:00', '2025-12-04 16:54:46'),
+(3, 'Guimel', 'Bustillos Landa', 'Guimel', '$2y$10$aG8oxH0e0Mpsdm29QN7l5.lK33//YavpZYP0QBCi6pkqIlLahZBPq', 'EMPLEADO', 'Guimel@gmail.com', '6672334455', 'ACTIVO', 'Empleado', '9:00-18:00', '2025-12-04 16:56:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimientos_stock`
+--
+
+CREATE TABLE `movimientos_stock` (
+  `id_movimiento` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `tipo` enum('ENTRADA','SALIDA') NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `motivo` varchar(150) DEFAULT NULL,
+  `referencia` varchar(50) DEFAULT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notificaciones`
+--
+
+CREATE TABLE `notificaciones` (
+  `id_notificacion` int(11) NOT NULL,
+  `id_empleado_destino` int(11) DEFAULT NULL,
+  `tipo` enum('STOCK_MINIMO','STOCK_MAXIMO') NOT NULL,
+  `mensaje` varchar(255) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `leida` tinyint(1) NOT NULL DEFAULT 0,
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ordenes_proveedor`
+--
+
+CREATE TABLE `ordenes_proveedor` (
+  `id_orden` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `monto_total` decimal(10,2) NOT NULL,
+  `nombre_empresa_emisora` varchar(150) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `tipo_orden` enum('NORMAL','INICIAL') NOT NULL DEFAULT 'NORMAL'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock_actual` int(11) NOT NULL DEFAULT 0,
+  `stock_minimo` int(11) NOT NULL DEFAULT 0,
+  `stock_maximo` int(11) NOT NULL DEFAULT 0,
+  `id_proveedor` int(11) NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estatus` enum('ACTIVO','INACTIVO') NOT NULL DEFAULT 'ACTIVO'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id_proveedor` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `empresa` varchar(150) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `correo` varchar(150) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `categoria` varchar(100) DEFAULT NULL,
+  `estatus` enum('ACTIVO','INACTIVO') NOT NULL DEFAULT 'ACTIVO'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas_prueba`
+--
+
+CREATE TABLE `ventas_prueba` (
+  `id_venta` int(11) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `total` decimal(10,2) NOT NULL,
+  `nombre_cliente` varchar(150) NOT NULL,
+  `id_empleado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `detalle_orden_proveedor`
+--
+ALTER TABLE `detalle_orden_proveedor`
+  ADD PRIMARY KEY (`id_detalle`),
+  ADD KEY `id_orden` (`id_orden`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `detalle_venta_prueba`
+--
+ALTER TABLE `detalle_venta_prueba`
+  ADD PRIMARY KEY (`id_detalle`),
+  ADD KEY `id_venta` (`id_venta`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
+  ADD UNIQUE KEY `correo` (`correo`),
+  ADD KEY `idx_empleados_filtros` (`rol`,`usuario`,`correo`,`telefono`);
+
+--
+-- Indices de la tabla `movimientos_stock`
+--
+ALTER TABLE `movimientos_stock`
+  ADD PRIMARY KEY (`id_movimiento`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD PRIMARY KEY (`id_notificacion`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_empleado_destino` (`id_empleado_destino`);
+
+--
+-- Indices de la tabla `ordenes_proveedor`
+--
+ALTER TABLE `ordenes_proveedor`
+  ADD PRIMARY KEY (`id_orden`),
+  ADD KEY `id_proveedor` (`id_proveedor`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `idx_ordenes_fecha_proveedor` (`fecha`,`id_proveedor`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_proveedor` (`id_proveedor`),
+  ADD KEY `idx_productos_nombre_categoria` (`nombre`,`categoria`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id_proveedor`),
+  ADD KEY `idx_proveedores_nombre_empresa` (`nombre`,`empresa`);
+
+--
+-- Indices de la tabla `ventas_prueba`
+--
+ALTER TABLE `ventas_prueba`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `idx_ventas_prueba_fecha` (`fecha`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_orden_proveedor`
+--
+ALTER TABLE `detalle_orden_proveedor`
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta_prueba`
+--
+ALTER TABLE `detalle_venta_prueba`
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `movimientos_stock`
+--
+ALTER TABLE `movimientos_stock`
+  MODIFY `id_movimiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  MODIFY `id_notificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ordenes_proveedor`
+--
+ALTER TABLE `ordenes_proveedor`
+  MODIFY `id_orden` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas_prueba`
+--
+ALTER TABLE `ventas_prueba`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_orden_proveedor`
+--
+ALTER TABLE `detalle_orden_proveedor`
+  ADD CONSTRAINT `detalle_orden_proveedor_ibfk_1` FOREIGN KEY (`id_orden`) REFERENCES `ordenes_proveedor` (`id_orden`),
+  ADD CONSTRAINT `detalle_orden_proveedor_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
+
+--
+-- Filtros para la tabla `detalle_venta_prueba`
+--
+ALTER TABLE `detalle_venta_prueba`
+  ADD CONSTRAINT `detalle_venta_prueba_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `ventas_prueba` (`id_venta`),
+  ADD CONSTRAINT `detalle_venta_prueba_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
+
+--
+-- Filtros para la tabla `movimientos_stock`
+--
+ALTER TABLE `movimientos_stock`
+  ADD CONSTRAINT `movimientos_stock_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
+
+--
+-- Filtros para la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
+  ADD CONSTRAINT `notificaciones_ibfk_2` FOREIGN KEY (`id_empleado_destino`) REFERENCES `empleados` (`id_empleado`);
+
+--
+-- Filtros para la tabla `ordenes_proveedor`
+--
+ALTER TABLE `ordenes_proveedor`
+  ADD CONSTRAINT `ordenes_proveedor_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id_proveedor`),
+  ADD CONSTRAINT `ordenes_proveedor_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id_proveedor`);
+
+--
+-- Filtros para la tabla `ventas_prueba`
+--
+ALTER TABLE `ventas_prueba`
+  ADD CONSTRAINT `ventas_prueba_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
